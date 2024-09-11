7c51547219e2af795238472145aa179ea15f195a
Curtis <shuman.curtis.cs@gmail.com>
2024-09-11 10:37:27 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -13,5 +13,18 @@ if (Operation == 1){
 }else if (Operation == 2){
     int Result = N1-N2;
     printf("The result of subtracting %d and %d is %d\n", N1, N2, Result);
-    return 0;
+}else if (Operation == 3){
+    int Result = N1*N2;
+    printf("The result of multiplying %d and %d is %d\n", N1, N2, Result);
+}else if (Operation == 4){
+    if(N2 !=0){
+    float Result = N1/N2;
+    printf("The result of multiplying %d and %d is %d\n", N1, N2, Result);
+    } else {
+        printf("division by zero is not allowed.\n")
+    }
+}
+
+
+return 0;
 }
