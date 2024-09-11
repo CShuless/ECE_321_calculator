2b5048e54f4b47da11f680f3c8ff8f3b68cb3cff
Curtis <shuman.curtis.cs@gmail.com>
2024-09-11 10:29:23 -0500

    Implemented addition feature

---
+++ calculator.c
@@ -0,0 +1,14 @@
+// Lab2 Simple Calculator
+// Curtis Shuman
+
+# include <stdio.h>
+
+int main() {
+
+    int N1, N2, Operation;
+
+    int Result = N1+N2;
+    printf("The result of adding %d and %d is %d\n", N1, N2, Result);
+
+    return 0;
+}
