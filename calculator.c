d43b6963a6346b70ba2cdcb03aa770e01f511b2d
Curtis <shuman.curtis.cs@gmail.com>
2024-09-11 10:31:15 -0500

    Implemented subtraction feature

--- calculator.c
+++ calculator.c
@@ -7,8 +7,11 @@ int main() {
 
     int N1, N2, Operation;
 
+if (Operation == 1){
     int Result = N1+N2;
     printf("The result of adding %d and %d is %d\n", N1, N2, Result);
-
+}else if (Operation == 2){
+    int Result = N1-N2;
+    printf("The result of subtracting %d and %d is %d\n", N1, N2, Result);
     return 0;
 }
